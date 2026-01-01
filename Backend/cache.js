220b5fae8a3f78fff070302ad2309163216d6b72
Shubham3066 <145591277+Shubham3066@users.noreply.github.com>
2026-01-01 12:17:15 +0530

    Update cache.js

--- Backend/cache.js
+++ Backend/cache.js
@@ -1,8 +1,9 @@
 const { LRUCache } = require("lru-cache");
 
 const cache = new LRUCache({
-  max: 100, // max 100 items
-  ttl: 1000 * 60 * 10, // 10 minutes
+  max: 100, 
+  ttl: 1000 * 60 * 10, 
 });
 
 module.exports = cache;
+
